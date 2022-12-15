38d189ea13fe18cacb7d954999d5b534fc1c46a8
Alexei <alexei.prudnikov@gmail.com>
2022-12-15 18:14:39 +0600

    add package InMemoryModel

--- src/InMemoryModel/ModelStore.java
+++ src/InMemoryModel/ModelStore.java
@@ -1,4 +1,50 @@
 package InMemoryModel;
 
-public class ModelStore {
+import ModelElements.Camera;
+import ModelElements.Flash;
+import ModelElements.PoligonalModel;
+import ModelElements.Scene;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModelStore implements IModelChanger {
+    public List<PoligonalModel> Models;
+    public List<Scene> Scenes;
+    public List<Flash> Flashes;
+    public List<Camera> Cameras;
+    private List<IModelChangedObserver> ChangeObservers;
+
+    public ModelStore(List<PoligonalModel> Models, List<Scene> Scenes, List<Flash> Flashes, List<Camera> Cameras) {
+        this.Models = Models;
+        this.Scenes = Scenes;
+        this.Flashes = Flashes;
+        this.Cameras = Cameras;
+        ChangeObservers = new ArrayList<>();
+    }
+
+    /**
+     * Возвращает scena по ID
+     *
+     * @param ID
+     * @return
+     */
+    public Scene GetScena(int ID) {
+        for (int i = 0; i < Scenes.size(); i++) {
+            if (Scenes.get(i).ID == ID) {
+                return Scenes.get(i);
+            }
+
+        }
+        return null;
+    }
+
+    /**
+     * Регистрация изменений
+     * @param sender
+     */
+    @Override
+    public void NotifyChange(IModelChanger sender) {
+
+    }
 }
